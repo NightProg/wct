868d47d47e53a6cb073eb2c0083b8ada7ceb688a
b4b4 <b4b4.ac@gmail.com>
2022-06-29 20:56:34 +0200

    recuperation of username

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import sys
 import src.search as search
 
 
+
 if sys.argv[1] == "-s":  # python run.py -s ...
-    print(search.get_argv_data(sys.argv))
+    print(search.searching(sys.argv))
 
 else:
     print(f"\"{' '.join(sys.argv[1:])}\" is not recognized.")  # "a b c d" is not recognized.
--- src/search.py
+++ src/search.py
@@ -1,6 +1,31 @@
 import os
-import ignore_analyser as ignore
+from . import ignore_analyser as ignore
 
 
-def get_argv_data(argv_data):
-    return f"{argv_data} : from SEARCH.PY !"
+def searching(argv_params):
+    os.chdir('/home/')
+    
+    # this part of the program allows to register the username of our user
+    # to be able to access his whole file tree 
+    users = {}
+
+    for user in os.listdir():
+        if user == ".directory":
+            continue
+        i = 1
+        users[str(i)] = user
+        i += 1
+
+    for key, user in users.items():
+        print(f"[{key}] : {user}")
+    
+    user_id = input('Who are you ? ')
+
+    try:
+        username = users[user_id]
+    except:
+        print(f"\nNo user associated with key {user_id}")
+        quit()
+    
+    return f"Welcome user {users[user_id]} !" 
+
