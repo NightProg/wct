f7a8972dc1f5e105ec0607525d3ac11bc90135c6
b4b4 <b4b4.ac@gmail.com>
2022-06-30 20:48:42 +0200

    params_verification adding

---
+++ src/module/params_verification.py
@@ -0,0 +1,27 @@
+def verification(argv_list):
+
+    if type(argv_list) != 'list':
+        print('The parameter of the verification() function is not an array as expected.')
+        quit()
+
+    if len(argv_list) < 3:
+        print('All the parameters did not pass, try again with all the parameters this time.\n
+               Refer to the README for more information about the parameters.')
+        quit()
+    
+    params_list = {
+        '-s': [
+            '--root',
+            '--session',
+            '--anywhere'
+        ]
+    }
+
+    for key, suffixes in params_list.items():
+        if key == argv_list[1]:
+            for suffix in suffixes:
+                if suffix == argv_list[2]:
+                    return True
+            print(f"{argv_list[1]} has not attribute \"{argv_list[2:0]}\"")
+            quit()
+
