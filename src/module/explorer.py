c2d422a44e6327e173e1027f9913393672e747ea
b4-b4 <b4b4.ac@gmail.com>
2022-07-02 09:43:12 +0200

    Reordering explorer

--- src/module/explorer.py
+++ src/module/explorer.py
@@ -10,7 +10,7 @@ def explorer(target: str):
         print(os.listdir())
         print(startin_path)
 
-        for item in os.listdir():
+        for item in os.listdir(os.getcwd()):
             # ending definition
             if os.path.isdir(''.join((os.getcwd(), "/", item))):
                 print("[is folder]")
@@ -20,23 +20,23 @@ def explorer(target: str):
                 ending = ""
 
             item_path = ''.join((os.getcwd(), item, ending))
-             
+ 
+            if os.getcwd() == startin_path and item == os.listdir()[-1] and item_path in visited_path:
+                return target_path
+
             if item == os.listdir()[-1] and item_path in visited_path:
                 print("exit..")
                 os.chdir('../')
-            
-            if os.getcwd() == startin_path and item == os.listdir()[-1] and item_path in visited_path:
-                return target_path
 
             if target in item and item_path not in target_path: 
                 print("found!")
                 target_path.append(item_path)
-            
+
             if os.path.isdir(item_path) and item_path not in visited_path:
                 print("enter..")
                 visited_path.append(item_path)
                 os.chdir(item_path)
+                continue
 
             if os.path.isdir(item_path) == False and item_path not in visited_path:
                 visited_path.append(item_path)
-            
