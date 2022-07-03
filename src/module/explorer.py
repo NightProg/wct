77c39f497fc0cb1d6b7bca4904f37c49a71dd419
b4b4 <b4b4.ac@gmail.com>
2022-07-03 21:13:45 +0200

    Last Commit of This Explorer

--- src/module/explorer.py
+++ src/module/explorer.py
@@ -4,63 +4,62 @@ import os, time
 def explorer(target: str, username):
     target_path = []
     visited_path = []
-    startin_path = os.getcwd()
+    starting_path = os.getcwd()
     current_folder = os.getcwd()
 
-    while True:
 
-        print(f"\n[BEFORE ENTERING, HERE ARE THE CURRENT FOLDER] : {current_folder}")
-        for item in os.listdir(current_folder):
+    for item in os.listdir(current_folder):
             
-            print(f"[ITEM] : {item}")
+        print(f"[ITEM] : {item}")
 
-            if os.path.isdir(''.join((os.getcwd(), "/", item))):
-                print(f"[is folder] : {''.join((os.getcwd(), '/', item))}")
-                ending = "/"
-            elif os.path.isfile(''.join((os.getcwd(), "/", item))):
-                print(f"[is file] : {''.join((os.getcwd(), '/', item))}")
-                ending = ""
-            else:
-                print("[Error]")
-
-            if os.getcwd() == '/':
-                item_path = ''.join((os.getcwd(), item, ending))
-            else:
-                item_path = ''.join((os.getcwd()+"/", item, ending))
-
-
-            # DEBUG: item_path + current_folder
-            print(f"[ITEM PATH] : {item_path}")
-            print(f"[CURRENT PATH] : {os.getcwd()}")
+        if os.path.isdir(''.join((os.getcwd(), "/", item))):
+            print(f"[is folder] : {''.join((os.getcwd(), '/', item))}")
+            ending = "/"
+        elif os.path.isfile(''.join((os.getcwd(), "/", item))):
+            print(f"[is file] : {''.join((os.getcwd(), '/', item))}")
+            ending = ""
+        else:
+            print("[Error]")
+            ending = ""
+                
+        if os.getcwd() == '/':
+            item_path = ''.join((os.getcwd(), item, ending))
+        else:
+            item_path = ''.join((os.getcwd()+"/", item, ending))
 
 
-            # The target is part of the item and has not yet been found
-            if target in item and item_path not in target_path:
-                target_path.append(item_path)
+        # DEBUG: item_path + current_folder
+        print(f"[ITEM PATH] : {item_path}")
+        print(f"[CURRENT PATH] : {os.getcwd()}")
 
-                # DEBUG: Target Path
-                print(f"[TARGET PATH] : {len(target_path)} ->")
-                for i in target_path:
-                    print(f"    > {i}")
+
+        # The target is part of the item and has not yet been found
+        if target in item and item_path not in target_path:
+            target_path.append(item_path)
+
+            # DEBUG: Target Path
+            print(f"[TARGET PATH] : {len(target_path)} ->")
+            for i in target_path:
+                print(f"    > {i}")
            
-            if item_path not in visited_path:
-                visited_path.append(item_path)
+        if item_path not in visited_path:
+            visited_path.append(item_path)
 
-                # DEBUG: Visited Path
-                print(f"[VISITED PATH] : {len(visited_path)} ->")
-                for i in visited_path:
-                    print(f"    > {i}")
-                print("\n")
+            # DEBUG: Visited Path
+            print(f"[VISITED PATH] : {len(visited_path)} ->")
+            for i in visited_path:
+                print(f"    > {i}")
+            print("\n")
 
-                if os.path.isdir(item_path):
-                    os.chdir(item_path)
-                    current_folder = item_path
-                    print("^ [ENTRY...]\n")
-                    continue
+            if os.path.isdir(item_path):
+                os.chdir(item_path)
+                current_folder = item_path
+                print("^ [ENTRY...]\n")
+                continue
 
-            if os.listdir(os.getcwd())[-1] == item and item_path in visited_path:
-               os.chdir("../")
-               current_folder = os.getcwd()
-               continue
+        if os.listdir(os.getcwd())[-1] == item and item_path in visited_path:
+            os.chdir("../")
+            current_folder = os.getcwd()
+            continue
 
-            time.sleep(0.1)
+        time.sleep(0.1)
