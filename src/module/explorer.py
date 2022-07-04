5ea720515efda6cb7715a4df84acea7fc729f230
b4b4 <b4b4.ac@gmail.com>
2022-07-04 15:23:20 +0200

    Successful: Explorer Work, with default

--- src/module/explorer.py
+++ src/module/explorer.py
@@ -1,66 +1,44 @@
-import os, time
+import os, sys
 
 
-def explorer(target: str, username):
-    target_path = []
-    visited_path = []
-    startin_path = os.getcwd()
-    current_folder = os.getcwd()
+class Explorer:
+    
+    def __init__(self):
+        self.file_count = 0
+        self.folder_count = 0
+        self.target_paths = []
 
-    while True:
+    def set_statistics(self, item):
+        if os.path.isdir(item):
+            self.folder_count += 1
 
-        print(f"\n[BEFORE ENTERING, HERE ARE THE CURRENT FOLDER] : {current_folder}")
-        for item in os.listdir(current_folder):
-            
-            print(f"[ITEM] : {item}")
+        elif os.path.isfile(item):
+            self.file_count += 1
 
-            if os.path.isdir(''.join((os.getcwd(), "/", item))):
-                print(f"[is folder] : {''.join((os.getcwd(), '/', item))}")
-                ending = "/"
-            elif os.path.isfile(''.join((os.getcwd(), "/", item))):
-                print(f"[is file] : {''.join((os.getcwd(), '/', item))}")
-                ending = ""
+    def get_statistics(self):
+        return f"{self.folder_count} folders, {self.file_count} files."
+
+    def explorer(self, directory, target, prefix=""):
+        list_paths = sorted([path for path in os.listdir(directory)])
+
+        for index in range(len(list_paths)):
+            if target in list_paths[index]:
+                self.target_paths.append((directory + list_paths[index]))
+
+            absolute = os.path.join(directory, list_paths[index])
+            self.set_statistics(absolute)
+
+            if index == len(list_paths) - 1:
+                print(prefix + "└── " + list_paths[index])
+                if os.path.isdir(absolute):
+                    self.explorer(absolute, "yarn", prefix + "    ")
             else:
-                print("[Error]")
+                print(prefix + "├── " + list_paths[index])
+                if os.path.isdir(absolute):
+                    self.explorer(absolute, "yarn", prefix + "│   ")
 
-            if os.getcwd() == '/':
-                item_path = ''.join((os.getcwd(), item, ending))
-            else:
-                item_path = ''.join((os.getcwd()+"/", item, ending))
-
-
-            # DEBUG: item_path + current_folder
-            print(f"[ITEM PATH] : {item_path}")
-            print(f"[CURRENT PATH] : {os.getcwd()}")
-
-
-            # The target is part of the item and has not yet been found
-            if target in item and item_path not in target_path:
-                target_path.append(item_path)
-
-                # DEBUG: Target Path
-                print(f"[TARGET PATH] : {len(target_path)} ->")
-                for i in target_path:
-                    print(f"    > {i}")
-           
-            if item_path not in visited_path:
-                visited_path.append(item_path)
-
-                # DEBUG: Visited Path
-                print(f"[VISITED PATH] : {len(visited_path)} ->")
-                for i in visited_path:
-                    print(f"    > {i}")
-                print("\n")
-
-                if os.path.isdir(item_path):
-                    os.chdir(item_path)
-                    current_folder = item_path
-                    print("^ [ENTRY...]\n")
-                    continue
-
-            if os.listdir(os.getcwd())[-1] == item and item_path in visited_path:
-               os.chdir("../")
-               current_folder = os.getcwd()
-               continue
-
-            time.sleep(0.1)
+research = Explorer()
+result = research.explorer("/home/b4b4/", "yarn")
+print(result)
+print(research.target_paths)
+print(research.get_statistics())
